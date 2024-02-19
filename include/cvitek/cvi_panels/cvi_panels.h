2e4138c2c7b87cf7e3ea9e0376c06626b8452d8e
Lu Hui <luhui@sipeed.com>
2024-02-19 18:59:33 +0800

    add mipi panel st7701_hd22801c31

--- include/cvitek/cvi_panels/cvi_panels.h
+++ include/cvitek/cvi_panels/cvi_panels.h
@@ -114,6 +114,15 @@ static struct panel_desc_s panel_desc = {
 	.dsi_init_cmds = dsi_init_cmds_st7701_480x800,
 	.dsi_init_cmds_size = ARRAY_SIZE(dsi_init_cmds_st7701_480x800)
 };
+#elif defined(MIPI_PANEL_ST7701_HD228001C31)
+#include "dsi_st7701_hd228001c31.h"
+static struct panel_desc_s panel_desc = {
+        .panel_name = "ST7701-368x552",
+        .dev_cfg = &dev_cfg_st7701_368x552,
+        .hs_timing_cfg = &hs_timing_cfg_st7701_368x552,
+        .dsi_init_cmds = dsi_init_cmds_st7701_368x552,
+        .dsi_init_cmds_size = ARRAY_SIZE(dsi_init_cmds_st7701_368x552)
+};
 #elif defined(MIPI_PANEL_ST7785M)
 #include "dsi_st7785m.h"
 static struct panel_desc_s panel_desc = {
