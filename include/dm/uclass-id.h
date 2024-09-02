d70fe28e80a2ee697e7beef381fc6e4d47715c33
James Deng <james.deng@spacemit.com>
2024-09-02 15:34:14 +0800

    Update for v2.0rc5

--- include/dm/uclass-id.h
+++ include/dm/uclass-id.h
@@ -38,6 +38,7 @@ enum uclass_id {
 	UCLASS_AXI,		/* AXI bus */
 	UCLASS_BLK,		/* Block device */
 	UCLASS_BATTERY,		/* Battery */
+	UCLASS_CHARGER,		/* Charger */
 	UCLASS_BOOTCOUNT,       /* Bootcount backing store */
 	UCLASS_BOOTDEV,		/* Boot device for locating an OS to boot */
 	UCLASS_BOOTMETH,	/* Bootmethod for booting an OS */
