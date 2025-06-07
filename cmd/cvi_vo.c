96fe33a941ec4170e8d88976235b736bc86be263
scpcom <scpcom@gmx.de>
2025-06-07 21:57:47 +0200

    cmd: cvi_vo: add alias for st7701_dxq5d0019_v0

--- cmd/cvi_vo.c
+++ cmd/cvi_vo.c
@@ -329,7 +329,8 @@ static int do_startvo(struct cmd_tbl *cmdtp, int flag, int argc, char * const ar
 			panel_desc.hs_timing_cfg = &hs_timing_cfg_st7701_480x854dxq;
 			panel_desc.dsi_init_cmds = dsi_init_cmds_st7701_480x854dxq;
 			panel_desc.dsi_init_cmds_size = ARRAY_SIZE(dsi_init_cmds_st7701_480x854dxq);
-		} else if (strcmp(panel_name,"st7701_dxq5d0019_V0") == 0) { // 5inch-new
+		} else if ((strcmp(panel_name,"st7701_dxq5d0019_v0") == 0) ||
+			   (strcmp(panel_name,"st7701_dxq5d0019_V0") == 0)) { // 5inch-new
 			panel_desc.panel_name = "ST7701-480x854dxq_V0";
 			panel_desc.dev_cfg = &dev_cfg_st7701_480x854dxq_V0,
 			panel_desc.hs_timing_cfg = &hs_timing_cfg_st7701_480x854dxq_V0,
