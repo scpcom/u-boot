206909b64f4f780e8c296d3321a558c3e3e13d9d
scpcom <scpcom@gmx.de>
2025-11-01 03:51:01 +0100

    cmd: kvm_hw_init: add kvm_hw and kvm_oled to bootargs

--- cmd/cvi_vo.c
+++ cmd/cvi_vo.c
@@ -393,6 +393,18 @@ static void kvm_hw_init(void)
 		return;
 	}
 
+	char *_bootargs = NULL;
+	char new_bootargs[256] = {0};
+	printf("boot key pressed\n");
+	_bootargs = env_get("othbootargs");
+	memcpy(new_bootargs, _bootargs, strlen(_bootargs));
+
+	char kvm_hw_arg[64] = {0};
+	sprintf(kvm_hw_arg, " kvm_hw=%s", kvm_hw);
+	memcpy(new_bootargs + strlen(new_bootargs), kvm_hw_arg, strlen(kvm_hw_arg));
+	printf("new_othbootargs[%ld]: %s\n", strlen(new_bootargs), new_bootargs);
+	env_set("othbootargs", new_bootargs);
+
 	char *kvm_oled = NULL;
 	if(get_value_from_header("kvm", "oled", buff, sizeof(buff)) == 0)
 		kvm_oled = buff;
@@ -406,6 +418,12 @@ static void kvm_hw_init(void)
 	if (!oled_exists) {
 		return;
 	}
+
+	char kvm_oled_arg[64] = {0};
+	sprintf(kvm_oled_arg, " kvm_oled=%s", kvm_oled);
+	memcpy(new_bootargs + strlen(new_bootargs), kvm_oled_arg, strlen(kvm_oled_arg));
+	printf("new_othbootargs[%ld]: %s\n", strlen(new_bootargs), new_bootargs);
+	env_set("othbootargs", new_bootargs);
 
 	oled_show_string("Loading");
 }
